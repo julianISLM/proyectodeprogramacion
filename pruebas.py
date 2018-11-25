cd8595b0c30827b070f55e74e1112b6aa2185548
Julian Orlando Medina Martínez <44948832+julianISLM@users.noreply.github.com>
2018-11-25 17:00:16 -0500

    Update pruebas.py
    Se mejoro el ejemplo dado que se agrego la función  stack_sick

--- pruebas.py
+++ pruebas.py
@@ -1,18 +1,28 @@
 import stack
 import cita
 
-pac = cita.Cita("Julian Orlando", "Medina Martìnez", "22", "16/11/2018", "Gripa", "Acetaminofen")
+pac = cita.Cita("Julian Orlando", "Medina Martìnez", "22", "16/11/2018",
+                "Gripa", "Acetaminofen")
 
 date = stack.Stack()
 receta = stack.Stack()
+sick = stack.Stack()
+
 
 def stack_date(x):
-    '''Agrega al Stack 'date' del paciente x las diferentes fechas en las que vino'''
+    '''Agrega al Stack 'date' del paciente las diferentes fechas en las que vino'''
     date.push(x.saveDate())
+
     return str(date)
+
+def stack_sick(x):
+    """agrega al stack 'sick' las enfermedades que ha tenido el paciente."""
+    sick.push(x.saveEnfermedad())
+    return str(sick)
+
     
 def stack_receta(x):
-    '''Agrega al Stack 'receta' las recetas del paciente x'''
+    '''Agrega al Stack 'receta' las recetas del paciente'''
     receta.push(x.saveReceta())
     return str(receta)
 
@@ -21,30 +31,41 @@ def stack_receta(x):
 ## el primer día que vinó.
 stack_date(pac)
 stack_receta(pac)
+stack_sick(pac)
 
 
 
 ##  segunda cita que vino el mismo paciente
+date.peek()
+sick.peek()
+receta.peek()
 
 pac.setFecha("18/11/2018")
 pac.setReceta("nada, porque se mejoró de la gripa.")
+pac.setEnfermedad("nada")
 stack_date(pac)
-stack_receta(pac) 
+stack_receta(pac)
+stack_sick(pac)
+
+
 
 
 ##tercera cita 
 ##el doctor quiere observar que fue lo último que se le receto al paciente y en que fecha
 
 ##aquí observa la cita anterior
-date.pop()
-receta.pop()
+date.peek()
+receta.peek()
+sick.peek()
 
-##Aquí observa la penultima cita
-date.pop()
-receta.pop()
-
+##Aquí observa la primera cita cita
+str(sick)
+str(date)
+str(receta)
 
 pac.setFecha("24/12/2018")
-pac.setReceta("Resulta que no era una gripa, era chikungunya. Se debe hospitalizar.")
+pac.setEnfermedad("Puede ser chikungunya, por el dolor en articulaciones")
+pac.setReceta("Se debe hospitalizar")
 stack_date(pac)
 stack_receta(pac)
+stack_sick(pac)
