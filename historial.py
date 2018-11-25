8084405b78804b03ad03a3d8079502ee8c433e97
david23mu <39017787+david23mu@users.noreply.github.com>
2018-11-25 12:34:44 -0500

    Add files via upload

---
+++ historial.py
@@ -0,0 +1,10 @@
+class Historial(cita, stack):
+    def __init__(self):
+        self.historial = []
+        self.historial.push(Cita.__init__(nombre)) 
+        self.historial.push(Cita.__init__(apellido))
+        self.historial.push(Cita.__init__(edad))
+        self.historial.push(Cita.__init__(fecha))
+        self.historial.push(Cita.__init__(enfermedad))
+        self.historial.push(Cita.__init__(receta))
+        return self.historial
