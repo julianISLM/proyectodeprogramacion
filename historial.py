5f6b37b70baaf305a91c788956581e127a8b55ea
Julian Orlando Medina Martínez <44948832+julianISLM@users.noreply.github.com>
2018-11-25 16:55:07 -0500

    Update historial.py
    Se le agrego la función str y se mejoro el código con la función ls que importa de la clase Cita.

--- historial.py
+++ historial.py
@@ -1,7 +1,20 @@
+import stack
+import cita
+
 class Historial:
-    def __init__(self, paciente, historial = stack.Stack()):
-        self.historial = historial
-        historial.push(paciente.ls())
+
+    def __init__(self, paciente):
+        """Crea un Stack y agrega directamente la primera cita del paciente"""
+        self.historial = stack.Stack()
+        self.historial.push(paciente.ls())
 
     def __str__(self):
+        """Retorna el stack craeado en tipo string"""
         return str(self.historial)
+
+    def addCita(self,paciente):
+        """Agrega un item al stack historial"""
+        self.historial.push(paciente.ls())
+        
+
+
