c09d7efd4e46a3e2a3a185e858529e1829cceee9
david23mu <39017787+david23mu@users.noreply.github.com>
2018-11-25 12:59:49 -0500

    Update historial.py

--- historial.py
+++ historial.py
@@ -1,10 +1,7 @@
 class Historial(cita, stack):
     def __init__(self):
         self.historial = []
-        self.historial.push(Cita.__init__(nombre)) 
-        self.historial.push(Cita.__init__(apellido))
-        self.historial.push(Cita.__init__(edad))
-        self.historial.push(Cita.__init__(fecha))
-        self.historial.push(Cita.__init__(enfermedad))
-        self.historial.push(Cita.__init__(receta))
+
+    def AddCita(self):
+        self.historial.push(Cita.All(DatosDeLaCita))
         return self.historial
