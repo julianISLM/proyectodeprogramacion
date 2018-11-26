f18c0a964328e11bda7d8d8308c9b64958b64def
david23mu <39017787+david23mu@users.noreply.github.com>
2018-11-26 14:26:47 -0500

    Update historial.py

--- historial.py
+++ historial.py
@@ -1,20 +1,43 @@
+import pydoc
 import stack
 import cita
 
 class Historial:
+    '''CLASE:
+-----------------------------------------------------------------------------------
+La clase Historial me permite almacenar los datos de cadaa cita por separado en
+forma de lista.
+-----------------------------------------------------------------------------------
+'''
 
     def __init__(self, paciente):
-        """Crea un Stack y agrega directamente la primera cita del paciente"""
+        """METODO: __init__
+Crea un Stack y agrega directamente la primera cita del paciente.
+VARIABLES: self y paciente
+"""
         self.historial = stack.Stack()
         self.historial.push(paciente.ls())
 
     def __str__(self):
-        """Retorna el stack craeado en tipo string"""
+        """METODO: __str__
+Retorna el stack craeado en tipo string.
+VARIABLES: self
+"""
         return str(self.historial)
 
     def addCita(self,paciente):
-        """Agrega un item al stack historial"""
+        """METODO: addCita
+Agrega un item al stack historial.
+VARIABLES: self y paciente
+"""
         self.historial.push(paciente.ls())
-        
+        inspect.getdoc(Cita)
+
+#DOCUMENTACION
+print(Historial.__doc__)
+print(Historial.__init__.__doc__)
+print(Historial.__str__.__doc__)
+print(Historial.addCita.__doc__)
+
 
 
